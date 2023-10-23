f897b8a3a97a6cd50a7aea3aacb6824da846529a
luckyklyist <puspaanupam@gmail.com>
2023-10-23 12:07:43 +0545

    interface added in the poke detail

--- src/Components/PokeDetail.tsx
+++ src/Components/PokeDetail.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-interface PokemonDetail {}
+interface PokemonDetail {
+  name: string;
+  height: number;
+  sprites: {
+    front_default: string;
+    back_default: string;
+    back_shiny: string;
+    front_shiny: string;
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+      home: {
+        front_default: string;
+      };
+    };
+  };
+}
 
 const PokeDetail = () => {
   const params = useParams<{ id: string }>();
-  const [pokemon, setPokemon] = React.useState([]);
+  const [pokemon, setPokemon] = React.useState<PokemonDetail>();
   React.useEffect(() => {
     const getPokemon = () => {
       fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
@@ -20,18 +37,18 @@ const PokeDetail = () => {
       <div className="grid grid-cols-2 gap-4 mb-4">
         <div className="text-center">
           <h2 className="text-xl font-semibold">Poke Name</h2>
-          <p className="text-lg">{pokemon.name}</p>
+          <p className="text-lg">{pokemon?.name}</p>
         </div>
         <div className="text-center">
           <h2 className="text-xl font-semibold">Poke Height</h2>
-          <p className="text-lg">{pokemon.height}</p>
+          <p className="text-lg">{pokemon?.height}</p>
         </div>
       </div>
       <div className="grid grid-cols-3 gap-4">
         <div className="flex flex-col items-center">
           <h2 className="text-xl font-semibold">Dream World</h2>
           <img
-            src={pokemon.sprites?.other.dream_world.front_default}
+            src={pokemon?.sprites?.other.dream_world.front_default}
             alt="Dream World"
             className="w-32 h-32"
           />
@@ -39,7 +56,7 @@ const PokeDetail = () => {
         <div className="flex flex-col items-center">
           <h2 className="text-xl font-semibold">Home</h2>
           <img
-            src={pokemon.sprites?.other.home.front_default}
+            src={pokemon?.sprites?.other.home.front_default}
             alt="Home"
             className="w-32 h-32"
           />
@@ -47,7 +64,7 @@ const PokeDetail = () => {
         <div className="flex flex-col items-center">
           <h2 className="text-xl font-semibold">Default</h2>
           <img
-            src={pokemon.sprites?.front_default}
+            src={pokemon?.sprites?.front_default}
             alt="Default"
             className="w-32 h-32"
           />
@@ -57,7 +74,7 @@ const PokeDetail = () => {
         <div className="flex flex-col items-center">
           <h2 className="text-xl font-semibold">Back Default</h2>
           <img
-            src={pokemon.sprites?.back_default}
+            src={pokemon?.sprites?.back_default}
             alt="Back Default"
             className="w-32 h-32"
           />
@@ -65,7 +82,7 @@ const PokeDetail = () => {
         <div className="flex flex-col items-center">
           <h2 className="text-xl font-semibold">Back Shiny</h2>
           <img
-            src={pokemon.sprites?.back_shiny}
+            src={pokemon?.sprites?.back_shiny}
             alt="Back Shiny"
             className="w-32 h-32"
           />
@@ -73,7 +90,7 @@ const PokeDetail = () => {
         <div className="flex flex-col items-center">
           <h2 className="text-xl font-semibold">Front Shiny</h2>
           <img
-            src={pokemon.sprites?.front_shiny}
+            src={pokemon?.sprites?.front_shiny}
             alt="Front Shiny"
             className="w-32 h-32"
           />
